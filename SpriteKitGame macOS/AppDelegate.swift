1311e4f2d5d4469e29f1a223db9b954e20d3acaf
Mark Szymczyk <mark@meandmark.com>
2018-09-13 15:45:18 -0400

    Initial Commit

---
+++ SpriteKitGame macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SpriteKitGame macOS
+//
+//  Created by mark on 9/13/18.
+//  Copyright © 2018 Me and Mark Publishing. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
